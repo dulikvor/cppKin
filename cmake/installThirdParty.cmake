5e87f011b032221d880b6964ca614a51a39e7372
Unknown <likdudi@gmail.com>
2017-11-05 11:33:53 -0800

    Build fixes.

--- cmake/installThirdParty.cmake
+++ cmake/installThirdParty.cmake
@@ -1,7 +1,7 @@
 include(ExternalProject) #In order to load the ExternalProject module and to add the ExternalProject_Add command
 set (EXTERNAL_PROJECT_DIR ${CMAKE_BINARY_DIR}/Third_Party CACHE PATH "External projects location") #Sets a variable with the installation location of all third party
 set_directory_properties(PROPERTIES EP_PREFIX ${EXTERNAL_PROJECT_DIR}) #Sets the prefix(dir) for all installations commands
-include(InstallBoost)
 include(InstallSpdLog)
 include(InstallThrift)
+include(InstallBoost)
 
