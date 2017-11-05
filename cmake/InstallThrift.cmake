4bc45d258f4f4427b259286c4867d9b074181550
Unknown <likdudi@gmail.com>
2017-11-05 12:54:51 -0800

    Build fixes.

--- cmake/InstallThrift.cmake
+++ cmake/InstallThrift.cmake
@@ -2,7 +2,7 @@ if (NOT THRIFT_FOUND)
     ExternalProject_Add(Thrift
             DOWNLOAD_NAME       thrift-0.10.0.tar.gz
             URL                 http://archive.apache.org/dist/thrift/0.10.0/thrift-0.10.0.tar.gz
-            CONFIGURE_COMMAND   cd <SOURCE_DIR> && ./configure --prefix=<INSTALL_DIR> --exec-prefix=<INSTALL_DIR> --without-go --without-python --
+            CONFIGURE_COMMAND   cd <SOURCE_DIR> && ./configure --prefix=<INSTALL_DIR> --exec-prefix=<INSTALL_DIR> --without-go --without-python --without-nodejs
             BUILD_COMMAND       make -C <SOURCE_DIR>
             INSTALL_COMMAND     make -C <SOURCE_DIR> install
             TEST_COMMAND        ""
