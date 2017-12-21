7ee70e347d89ddfb7d284c65c4e84c732b682e69
Roma Koifman <Roma.koifman@gmail.com>
2017-12-21 12:00:20 +0200

    adding example

--- examples/README.md
+++ examples/README.md
@@ -1,2 +1,11 @@
 # Readme
 
+Start Zipking server
+-------------------
+
+wget -O zipkin.jar 'https://search.maven.org/remote_content?g=io.zipkin.java&a=zipkin-server&v=LATEST&c=exec'
+java -jar zipkin.jar
+
+
+
+
--- examples/example.cpp
+++ examples/example.cpp
@@ -1,23 +1,55 @@
 #include <string>
+#include <boost/program_options.hpp>
 #include "src/cppkin.h"
 #include "Core/src/Logger.h"
 #include "Core/src/TraceListener.h"
 
-
 using namespace std;
 using namespace core;
+using namespace boost::program_options;
 
-int main()
+void on_age(int age)
 {
-    Logger::Instance().AddListener(make_shared<FileRotationListener>(TraceSeverity::Info, "./cppkinClient", 50 * 1024 * 1024, 20));
+  std::cout << "On age: " << age << '\n';
+}
+
+int main( int argc, const char *argv[] )
+{
+    options_description desc{"Options"};
+        desc.add_options()
+          ("help,h", "Help screen")
+          ("transport", value<string>()->default_value("http"), "Transport" )
+          ("host", value<string>()->default_value("127.0.0.1"), "Host" )
+          ("port", value<int>()->default_value(-1), "Port");
+
+    variables_map vm;
+    store(parse_command_line(argc, argv, desc), vm);
+    notify(vm);
+
+    if (vm.count("help")) {
+        std::cout << desc << '\n';
+        return 0;
+    }
+
+    Logger::Instance().AddListener(make_shared<FileRotationListener>(TraceSeverity::Info, "./example", 50 * 1024 * 1024, 20));
     Logger::Instance().Start(TraceSeverity::Info);
 
+    int port = 9441;
+    auto transportType = cppkin::TransportType::Http;
+    if (vm["transport"].as<string>() == "scribe") {
+        transportType = cppkin::TransportType::Scribe;
+        if (vm["port"].as<int>() == -1 ) {
+            port = 9440;
+        }
+    }
+
     cppkin::GeneralParams cppkinParams;
-    cppkinParams.AddParam(cppkin::ConfigTags::HOST_ADDRESS, string("127.0.0.1"));
-    cppkinParams.AddParam(cppkin::ConfigTags::PORT, 9410);
-    cppkinParams.AddParam(cppkin::ConfigTags::SERVICE_NAME, string("Cluster_Manager"));
+    cppkinParams.AddParam(cppkin::ConfigTags::HOST_ADDRESS, vm["host"].as<string>());
+    cppkinParams.AddParam(cppkin::ConfigTags::PORT, port);
+    cppkinParams.AddParam(cppkin::ConfigTags::SERVICE_NAME, string("example_service"));
     cppkinParams.AddParam(cppkin::ConfigTags::DEBUG, true);
     cppkinParams.AddParam(cppkin::ConfigTags::SAMPLE_COUNT, 1);
+    cppkinParams.AddParam(cppkin::ConfigTags::TRANSPORT_TYPE, cppkin::TransportType(transportType).ToString());
     INIT(cppkinParams);
 
     CREATE_TRACE("Test");
