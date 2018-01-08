b771e6082c986bef853e8f592a30d2f83d9cf0de
Unknown <likdudi@gmail.com>
2018-01-08 06:25:36 -0800

    Thrift installation update, Boost installation update.

--- cmake/InstallBoost.cmake
+++ cmake/InstallBoost.cmake
@@ -21,7 +21,7 @@ if (NOT BOOST_FOUND)
 						URL                 https://dl.bintray.com/boostorg/release/1.65.1/source/boost_1_65_1.tar.gz
 						CONFIGURE_COMMAND   cd <SOURCE_DIR> && ./bootstrap.sh --prefix=<INSTALL_DIR> --exec-prefix=<INSTALL_DIR>
 						BUILD_COMMAND       ""
-						INSTALL_COMMAND     cd <SOURCE_DIR> && ./b2 install 
+						INSTALL_COMMAND     cd <SOURCE_DIR> && ./b2 install --with-program_options
 						TEST_COMMAND        ""
 		)
 	endif()
