1577b19dc554d698fdbdc85d0eff53a133dab211
Unknown <likdudi@gmail.com>
2018-07-12 11:59:38 -0700

    Boost installation update.

--- cmake/FindBOOST.cmake
+++ cmake/FindBOOST.cmake
@@ -1,6 +1,6 @@
 set(BOOST_INCLUDEDIR ${PROJECT_DIR}/Third_Party/include)
 set(BOOST_LIBRARYDIR ${PROJECT_DIR}/Third_Party/lib)
-find_package(Boost 1.65.1 COMPONENTS program_options)
+find_package(Boost COMPONENTS program_options)
 
 if(Boost_FOUND)
     message(STATUS ${Green}"Found Boost include dir - ${Boost_INCLUDE_DIR}"${ColourReset})
