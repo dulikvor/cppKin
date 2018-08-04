7e52e7ffdb200fad50d444aea14b609ef015b02e
Unknown <likdudi@gmail.com>
2018-08-04 22:46:55 +0300

    missing file.

---
+++ src/cppkin.cpp
@@ -0,0 +1,33 @@
+#include "cppkin.h"
+#include "core/Environment.h"
+#include "core/Logger.h"
+#include "ConfigParams.h"
+#include "TransportManager.h"
+#include "SpanContainer.h"
+
+using namespace core;
+
+namespace cppkin
+{
+    void Init(const GeneralParams& params){
+        Environment::Instance().Init();
+        Logger::Instance().Start(TraceSeverity::Info);
+        ConfigParams::Instance().Load(params);
+    }
+
+    void Stop()
+    {
+        TransportManager::Instance().Stop();
+        Logger::Instance().Terminate();
+    }
+
+    void PushSpan(Span& span)
+    {
+        SpanContainer::Instance().PushSpan(span);
+    }
+
+    Span* const PopSpan()
+    {
+        return SpanContainer::Instance().PopSpan();
+    }
+}
