08e219306bd3cd7e4682f1f8fb1dfaf479968c9e
dudilikvornik <likdudi@gmail.com>
2019-04-09 23:06:53 -0700

    build fixes.

--- cmake/InstallRapidJson.cmake
+++ cmake/InstallRapidJson.cmake
@@ -5,19 +5,13 @@ if (NOT RAPIDJSON_FOUND)
 		LinuxPath_ToWinPath(${PROJECT_3RD_LOC}/src/RAPIDJSON SOURCE_DIR_WIN)
 		string(TOLOWER ${CMAKE_BUILD_TYPE} poco_build)
 		ExternalProject_Add(RAPIDJSON
-						DOWNLOAD_NAME       poco-1.8.1-release.tar.gz
-						URL                 https://github.com/pocoproject/poco/archive/poco-1.8.1-release.tar.gz
-						URL_MD5             e64735b06d2520ebf1bcc2b7ffe81562
+                        DOWNLOAD_NAME       RapidJSON-1.1.0.tar.gz
+                        URL                 https://github.com/miloyip/rapidjson/archive/v1.1.0.tar.gz
 						CONFIGURE_COMMAND   cd ${SOURCE_DIR_WIN} && echo Foundation > components && echo CppUnit >> components && echo JSON >> components
-						BUILD_COMMAND       cd ${SOURCE_DIR_WIN} && CMD /C buildwin.cmd 120 build shared ${poco_build} Win32 nosamples notests msbuild
-						INSTALL_COMMAND     if not exist ${INSTALL_DIR_WIN}\\include\\Poco\\JSON mkdir ${INSTALL_DIR_WIN}\\include\\Poco\\JSON && if not exist ${INSTALL_DIR_WIN}\\lib mkdir ${INSTALL_DIR_WIN}\\lib
+						BUILD_COMMAND       if not exist ${INSTALL_DIR_WIN}\\include\\rapidjson mkdir ${INSTALL_DIR_WIN}\\include\\rapidjson
+                        INSTALL_COMMAND     xcopy ${SOURCE_DIR_WIN}\\include\\rapidjson\\*.h ${INSTALL_DIR_WIN}\\include\\rapidjson /sy
 						TEST_COMMAND        ""
 		)
-				
-		ExternalProject_Add_Step(RAPIDJSON RAPIDJSON_Install_JSON_Headers
-				COMMAND     xcopy ${SOURCE_DIR_WIN}\\JSON\\include\\Poco\\JSON\\*.h ${INSTALL_DIR_WIN}\\include\\Poco\\JSON /E
-				DEPENDEES   install
-				)
     else()
         ExternalProject_Add(RAPIDJSON
                 DOWNLOAD_NAME       RapidJSON-1.1.0.tar.gz
--- cppkin.sh
+++ cppkin.sh
@@ -2,7 +2,7 @@
 
 display_help(){
     echo "                                                               "
-    echo "cppkin compile [options]..."
+    echo "cppkin config [options]..."
     echo "Install                       Install related options."
     echo "======="
     echo "--3rd_loc_prefix=<PREFIX>     3rd party install area prefix.   "
@@ -49,7 +49,7 @@ clean_cmake_cache(){
     done
 }
 
-compile(){
+config(){
     WITH_THRIFT=OFF
     WITH_TESTS=OFF
     WITH_EXAMPLES=OFF
@@ -83,7 +83,7 @@ compile(){
             ;;
         esac
     done
-    cmake -DPRE_COMPILE_STEP=ON -D3RD_PARTY_INSTALL_STEP=ON -DCOMPILATION_STEP=ON -DWITH_THRIFT=$WITH_THRIFT -D WITH_TESTS=$WITH_TESTS -DWITH_EXAMPLES=$WITH_EXAMPLES -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DPROJECT_3RD_LOC:STRING=$THIRD_PARTY_PREFIX -DOUTPUT_DIR:STRING=$OUTPUT_DIR -DPYTHON_BINDING:STRING=$PYTHON_BINDING . && make
+    cmake -DPRE_COMPILE_STEP=ON -D3RD_PARTY_INSTALL_STEP=ON -DCOMPILATION_STEP=ON -DWITH_THRIFT=$WITH_THRIFT -D WITH_TESTS=$WITH_TESTS -DWITH_EXAMPLES=$WITH_EXAMPLES -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DPROJECT_3RD_LOC:STRING=$THIRD_PARTY_PREFIX -DOUTPUT_DIR:STRING=$OUTPUT_DIR -DPYTHON_BINDING:STRING=$PYTHON_BINDING .
 }
 
 while true; do
@@ -97,8 +97,8 @@ while true; do
                 return 0
             fi
         ;;
-        compile)
-            compile $@
+        config)
+            config $@
             if [ "$0" = "$BASH_SOURCE" ]
             then
                 exit 0
@@ -116,7 +116,7 @@ while true; do
             fi
         ;;
         *)
-            echo "supported commands - --help, compile, clean_cache"
+            echo "supported commands - --help, config, clean_cache"
             if [ "$0" = "$BASH_SOURCE" ]
             then
                 exit 0
